1913b4034c0f1cb52ac0a4c88bdd64a9ffb1704f
Rafael Esaa <esaarafael@hotmail.com>
2019-05-31 14:24:51 -0400

    resolved issue in deploy netlify

--- src/html.js
+++ src/html.js
@@ -13,22 +13,22 @@ export default class HTML extends React.Component {
                     />
                     {this.props.headComponents}
                     <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
-                    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
                 </head>
-                    <body>
-                        <div
-                            id="___gatsby"
-                            dangerouslySetInnerHTML={{ __html: this.props.body }}
-                        />
-                        {this.props.postBodyComponents}
-                        <script
-                            async
-                            src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                        />
-                        <script src="https://unpkg.com/scrollreveal" />
-                    </body>
+                <body>
+                    <div
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{ __html: this.props.body }}
+                    />
+                    {this.props.postBodyComponents}
+                    <script
+                        async
+                        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    />
+                    <script src="https://unpkg.com/scrollreveal" />
+                </body>
             </html>
-                )
-            }
-        }
+        )
+    }
+}
