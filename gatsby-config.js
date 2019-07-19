81a9d5cdd31cc083877aa5951878066f74799419
Rafael Esaa <esaarafael@hotmail.com>
2019-07-19 15:43:49 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Borodigital`,
     description: `Digital Agency.`,
-    author: `github/RafaelEsaa`,
+    author: `github/RafaelEsaa`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,22 +10,22 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/data`,
-      },
+        path: `${__dirname}/src/data`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -42,25 +42,25 @@ module.exports = {
         // GitHub Flavored Markdown mode (default: true)
         gfm: true,
         // Plugins configs
-        plugins: [],
-      },
+        plugins: []
+      }
     },
     {
       resolve: `gatsby-source-instagram`,
       options: {
         username: `boro_digital`,
         access_token: "6679894764.1677ed0.3b3579d7b2dc4a12b3d9840f8566f0df",
-        instagram_id: "210ae3167b19424795309e8a88593292",
-      },
+        instagram_id: "210ae3167b19424795309e8a88593292"
+      }
     },
     {
-      resolve: 'gatsby-plugin-i18n',
+      resolve: "gatsby-plugin-i18n",
       options: {
-        langKeyDefault: 'en',
+        langKeyDefault: "en",
         useLangKeyLayout: false,
         prefixDefault: false,
         markdownRemark: {
-          postPage: 'src/templates/blog-post.js',
+          postPage: "src/templates/blog-post.js",
           query: `
             {
               allMarkdownRemark {
@@ -79,7 +79,7 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: `gatsby-plugin-sass`
     },
     //Plugin favicon
     {
@@ -91,11 +91,18 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo-boro.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/logo-boro.png` // This path is relative to the root of the site.
+      }
     },
+    //Metrics google analytics
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-144173674-1"
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
